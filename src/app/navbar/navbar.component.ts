ad27b82098f5c6f32ca0661d2ca009316afb773c
ferparrav <ferparrav@outlook.com>
2025-06-11 12:59:14 -0500

    Fix navbar auto collapse

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,13 +2,21 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
+
+declare var bootstrap: any;
+
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    collapseNavbar() {
+        const navbar = document.getElementById('navbarNav');
+        if (navbar?.classList.contains('show')) {
+            new bootstrap.Collapse(navbar).hide();
+        }
+    }
 }
